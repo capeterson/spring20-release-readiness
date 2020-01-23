92212c1937db68a15ea3952bae845e0ca93b402d
Chris Peterson <chris.peterson@salesforce.com>
2020-01-23 12:35:57 -0800

    Move from console.log to toast messages for speed of demo

--- force-app/main/default/lwc/quickcreate/quickcreate.js
+++ force-app/main/default/lwc/quickcreate/quickcreate.js
@@ -1,6 +1,6 @@
 import { LightningElement, api } from 'lwc';
 import create from '@salesforce/apex/QuickCreateController.create';
-import Opportunity from '@salesforce/schema/Opportunity';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class Quickcreate extends LightningElement {
     opp;
@@ -16,20 +16,36 @@ export default class Quickcreate extends LightningElement {
 
     handleFieldChange(event){
         let field = event.target.name;
-        this.opp[field] = event.target.value;
+        if(field === 'CloseDate'){
+            this.opp[field] = new Date(event.target.value);
+        }else{
+            this.opp[field] = event.target.value;
+        }
     }
 
     saveRecord(event) {
-        debugger;
+        //let's pretend an attacker set a javascript breakpoint here, and then tried to inject a custom field our controller didn't expect
+        this.opp.Manager_Approved__c = true;
+        //end injection attack
         create({opp: this.opp})
             .then(result => {
                 this.opp = result;
-                console.log('Saved opportunity record', this.opp);
+                let evt = new ShowToastEvent({
+                    title: 'Saved',
+                    message: 'Data saved successfully',
+                    variant: {label: 'success', value: 'success'},
+                });
+                this.dispatchEvent(evt);
                 event.target.disabled = true;
             })
             .catch(error => {
                 this.error = error;
-                console.error('Unexpected error saving opp:',error);
+                let evt = new ShowToastEvent({
+                    title: 'Error Saving Data',
+                    message: this.error.body.message,
+                    variant: {label: 'error', value: 'error'},
+                });
+                this.dispatchEvent(evt);
             });
     }
 
